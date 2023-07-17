86f325aecaf9bf4030b9fe605d6e28ba011ebb0f
Mochamad Irvan <111232719+mchmdirvan@users.noreply.github.com>
2023-07-17 09:31:52 +0700

    finished quiz 1 & 2

---
+++ quiz-2-string.dart
@@ -0,0 +1,7 @@
+void main() {
+  String greetings = "Good Morning, ";
+  String name = "Mr. Irvan! ";
+  String greetingsPlace = "Welcome to the world";
+
+  print(greetings + name + greetingsPlace);
+}
